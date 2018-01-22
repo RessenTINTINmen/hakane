97e47ed601f716059c53366dd8217f23b6a1dd10
Tetsuro <happy.sadaharu@docomo.ne.jp>
2018-01-23 05:02:59 +0900

    make it better

--- src/main/java/com/example/com/example/MySqlApplication.java
+++ src/main/java/com/example/com/example/MySqlApplication.java
@@ -1,7 +1,10 @@
 package com.example.com.example;
 
+import java.util.Arrays;
 import java.util.List;
 import java.util.Map;
+
+import javax.sql.DataSource;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
@@ -10,10 +13,18 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.jdbc.core.JdbcTemplate;
 
+import com.linecorp.bot.model.ReplyMessage;
+import com.linecorp.bot.model.action.Action;
+import com.linecorp.bot.model.action.PostbackAction;
 import com.linecorp.bot.model.event.Event;
 import com.linecorp.bot.model.event.MessageEvent;
+import com.linecorp.bot.model.event.PostbackEvent;
 import com.linecorp.bot.model.event.message.TextMessageContent;
+import com.linecorp.bot.model.event.postback.PostbackContent;
+import com.linecorp.bot.model.message.Message;
+import com.linecorp.bot.model.message.TemplateMessage;
 import com.linecorp.bot.model.message.TextMessage;
+import com.linecorp.bot.model.message.template.ButtonsTemplate;
 import com.linecorp.bot.spring.boot.annotation.EventMapping;
 import com.linecorp.bot.spring.boot.annotation.LineMessageHandler;
 
@@ -22,6 +33,10 @@ import com.linecorp.bot.spring.boot.annotation.LineMessageHandler;
 @ComponentScan
 @LineMessageHandler
 public class MySqlApplication {
+
+	private int id = 1, mode = 0;
+	private DataSource dataSource = null;
+	private String word = null, mean = null;
 
     @Autowired
     private JdbcTemplate jdbc;
@@ -33,26 +48,105 @@ public class MySqlApplication {
 
 
     @EventMapping
-    public TextMessage handleTextMessageEvent(MessageEvent<TextMessageContent> event) {
+    public ReplyMessage handleTextMessageEvent(MessageEvent<TextMessageContent> event) {
         System.out.println("event: " + event);//イベントをログに出力
 
         String inputstr = event.getMessage().getText();//入力された文字列をinputstrに入れる
-
-      	List<Map<String, Object>> list2 = jdbc.queryForList
-    			("SELECT means FROM instrument where words like '" + inputstr + "';");
-    	 //テーブル「instrument」から文字列「入力値」を含む行から、カラム「comment」を取り出す。
-
-        list2.forEach(System.out::println);   //取り出したカラムを文字列に変換。
-
-    	//以下で =以降の表示したい部分を決める。//
-        //作りが悪いだけであって、うまく書けば不要な部分です。多分。//
-  	String string = list2.toString();
-    	int start = string.indexOf("=") + 1;
-    	int end = string.indexOf("}");
-    	String new_str2 = string.substring(start,end);
-
-    	return new TextMessage(new_str2);  //new_str2を返す。
+        String msg1;
+        Object obj = null;
+        switch (mode) {
+        case 0:
+        		msg1 = "こんちわ	、なにかはかねにようじ？";
+        		String title = "こうどうせんたく";
+            Action addWords = new PostbackAction("コトバをオシえる", "addWords");
+            Action readWords = new PostbackAction("コトバをキく", "readWords");
+            Action readRecent = new PostbackAction("さいきんなにオボえた？", "readRecent");
+            List<Action> actions = Arrays.asList(addWords, readWords, readRecent);
+            ButtonsTemplate buttonsTemplate = new ButtonsTemplate(null, title, msg1, actions);
+        		String altText = title;
+        		obj = new TemplateMessage(altText, buttonsTemplate);
+        		break;
+        case 1:
+        		word = inputstr;
+        		mode = 2;
+        		msg1 = "それってどーゆーイミなの？";
+        		obj = Arrays.asList(new TextMessage(msg1));
+        		break;
+        case 2:
+        		mean = inputstr;
+        		mode = 0;
+        		createColumn(word, mean);
+        		msg1 = "オボえたよ。";
+        		obj = Arrays.asList(new TextMessage(msg1));
+        		List<Map<String, Object>> list2 = jdbc.queryForList
+    				("SELECT means FROM instrument where words like '" + inputstr + "';");
+        		//テーブル「instrument」から文字列「入力値」を含む行から、カラム「comment」を取り出す。
+        		list2.forEach(System.out::println);   //取り出したカラムを文字列に変換。
+        		//以下で =以降の表示したい部分を決める。//
+        		//作りが悪いだけであって、うまく書けば不要な部分です。多分。//
+        		String string = list2.toString();
+        		int start = string.indexOf("=") + 1;
+        		int end = string.indexOf("}");
+        		String msg = string.substring(start,end);
+        		msg1 = "	『" + inputstr + "』は『" + msg + "』ってイミだよ。";
+        		obj = Arrays.asList(new TextMessage(msg1));
+        		break;
+        }
+    	return new ReplyMessage(event.getReplyToken(), (Message) obj);
     	}
+
+    protected ReplyMessage handlePostbackEvent(PostbackEvent event) {
+        // ButtonsTemplateでユーザーが選択した結果が、このPostBackEventとして返ってくる
+
+        PostbackContent postbackContent = event.getPostbackContent();
+
+        // PostbackActionで設定したdataを取得する
+        String data = postbackContent.getData();
+        String word = null, mean = null;
+        String crlf = System.getProperty("line.separator");
+
+        final String replyText;
+
+        if ("addWords".equals(data)) {
+            replyText = "なにをオシえてくれるの？";
+            mode = 1;
+        } else if ("readWords".equals(data)) {
+            replyText = "なにがシりたい？";
+            mode = 3;
+        } else {
+        		List<Map<String, Object>> list1 = jdbc.queryForList
+        				("SELECT words FROM instrument where id like " + id + ";");
+        		List<Map<String, Object>> list2 = jdbc.queryForList
+        				("SELECT means FROM instrument where id like " + id + ";");
+        		//テーブル「instrument」から文字列「入力値」を含む行から、カラム「comment」を取り出す。
+        		list2.forEach(System.out::println);   //取り出したカラムを文字列に変換。
+
+        		//以下で =以降の表示したい部分を決める。//
+            //作りが悪いだけであって、うまく書けば不要な部分です。多分。//
+        		String string1 = list1.toString();
+        		int start1 = string1.indexOf("=") + 1;
+        		int end1 = string1.indexOf("}");
+        		word = string1.substring(start1,end1);
+        		String string2 = list2.toString();
+        		int start2 = string2.indexOf("=") + 1;
+        		int end2 = string2.indexOf("}");
+        		mean = string2.substring(start2,end2);
+            replyText = "さいきん『"+ word +"』をオボえたよ。" + crlf + "『" + mean + "』ってイミなんだって";
+        }
+
+        return new ReplyMessage(event.getReplyToken(), Arrays.asList(new TextMessage(replyText)));
+    }
+
+    public void createColumn(String word, String mean) {
+        final String sql = "INSERT INTO instrument (words, means) VALUES ('" + word + "', '" + mean + "');";
+        JdbcTemplate jt = new JdbcTemplate(this.dataSource);
+        jt.execute(sql);
+    }
+
+    public void setDataSource(DataSource dataSource) {
+        this.dataSource = dataSource;
+    }
+
 
     @EventMapping
     public void handleDefaultMessageEvent(Event event) {
