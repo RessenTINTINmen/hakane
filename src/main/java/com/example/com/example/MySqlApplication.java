b609fef01c353ab54fcce3b6d23cceff3bc6de74
Tetsuro <happy.sadaharu@docomo.ne.jp>
2018-01-23 06:05:53 +0900

    make it better

--- src/main/java/com/example/com/example/MySqlApplication.java
+++ src/main/java/com/example/com/example/MySqlApplication.java
@@ -34,9 +34,9 @@ import com.linecorp.bot.spring.boot.annotation.LineMessageHandler;
 @LineMessageHandler
 public class MySqlApplication {
 
-	private int id = 1, mode = 0;
+	private int id = 1/*, mode = 0*/;
 	private DataSource dataSource = null;
-	private String word = null, mean = null;
+	//private String word = null, mean = null;
 
     @Autowired
     private JdbcTemplate jdbc;
@@ -51,51 +51,50 @@ public class MySqlApplication {
     public ReplyMessage handleTextMessageEvent(MessageEvent<TextMessageContent> event) {
         System.out.println("event: " + event);//イベントをログに出力
 
-        String inputstr = event.getMessage().getText();//入力された文字列をinputstrに入れる
+        //String inputstr = event.getMessage().getText();//入力された文字列をinputstrに入れる
         String msg1;
-        TemplateMessage obj = null;
 
-        switch (mode) {
-        case 0:
+        //switch (mode) {
+        //case 0:
         		msg1 = "こんちわ	、なにかはかねにようじ？";
+        		String thumbnailImageUrl = "https://riversun.github.io/img/riversun_256.png";
         		String title = "こうどうせんたく";
             Action addWords = new PostbackAction("コトバをオシえる", "addWords");
             Action readWords = new PostbackAction("コトバをキく", "readWords");
             Action readRecent = new PostbackAction("さいきんなにオボえた？", "readRecent");
             List<Action> actions = Arrays.asList(addWords, readWords, readRecent);
-            ButtonsTemplate buttonsTemplate = new ButtonsTemplate(null, title, msg1, actions);
+            ButtonsTemplate buttonsTemplate = new ButtonsTemplate(thumbnailImageUrl, title, msg1, actions);
         		String altText = title;
-        		obj = new TemplateMessage(altText, buttonsTemplate);
-        		break;
-        case 1:
-        		word = inputstr;
-        		mode = 2;
-        		msg1 = "それってどーゆーイミなの？";
+        		return new ReplyMessage(event.getReplyToken(), (Message) new TemplateMessage(altText, buttonsTemplate));
+        //case 1:
+        //		word = inputstr;
+        //		mode = 2;
+        //		msg1 = "それってどーゆーイミなの？";
+        //		//obj = Arrays.asList(new TextMessage(msg1));
+        //		return new ReplyMessage(event.getReplyToken(), Arrays.asList(new TextMessage(msg1)));
+        //case 2:
+        //		mean = inputstr;
+        //		mode = 0;
+        //		createColumn(word, mean);
+        	//	msg1 = "オボえたよ。";
         		//obj = Arrays.asList(new TextMessage(msg1));
-        		break;
-        case 2:
-        		mean = inputstr;
-        		mode = 0;
-        		createColumn(word, mean);
-        		msg1 = "オボえたよ。";
-        		//obj = Arrays.asList(new TextMessage(msg1));
-        case 3:
-        		mode = 0;
-        		List<Map<String, Object>> list2 = jdbc.queryForList
-    				("SELECT means FROM instrument where words like '" + inputstr + "';");
-        		//テーブル「instrument」から文字列「入力値」を含む行から、カラム「comment」を取り出す。
-        		list2.forEach(System.out::println);   //取り出したカラムを文字列に変換。
-        		//以下で =以降の表示したい部分を決める。//
-        		//作りが悪いだけであって、うまく書けば不要な部分です。多分。//
-        		String string = list2.toString();
-        		int start = string.indexOf("=") + 1;
-        		int end = string.indexOf("}");
-        		String msg = string.substring(start,end);
-        		msg1 = "	『" + inputstr + "』は『" + msg + "』ってイミだよ。";
-        		//obj = Arrays.asList(new TextMessage(msg1));
-        		break;
-        }
-    	return new ReplyMessage(event.getReplyToken(), (Message) obj);
+        	//	return new ReplyMessage(event.getReplyToken(), Arrays.asList(new TextMessage(msg1)));
+        //case 3:
+        //		mode = 0;
+        //		List<Map<String, Object>> list2 = jdbc.queryForList
+    		//		("SELECT means FROM instrument where words like '" + inputstr + "';");
+        //		//テーブル「instrument」から文字列「入力値」を含む行から、カラム「comment」を取り出す。
+        //		list2.forEach(System.out::println);   //取り出したカラムを文字列に変換。
+        //		//以下で =以降の表示したい部分を決める。//
+        //		//作りが悪いだけであって、うまく書けば不要な部分です。多分。//
+        //		String string = list2.toString();
+        //		int start = string.indexOf("=") + 1;
+        //		int end = string.indexOf("}");
+        //		String msg = string.substring(start,end);
+        //		msg1 = "	『" + inputstr + "』は『" + msg + "』ってイミだよ。";
+        //		//obj = Arrays.asList(new TextMessage(msg1));
+        //		return new ReplyMessage(event.getReplyToken(), Arrays.asList(new TextMessage(msg1)));
+        //}
     	}
 
     protected ReplyMessage handlePostbackEvent(PostbackEvent event) {
@@ -112,10 +111,10 @@ public class MySqlApplication {
 
         if ("addWords".equals(data)) {
             replyText = "なにをオシえてくれるの？";
-            mode = 1;
+            //mode = 1;
         } else if ("readWords".equals(data)) {
             replyText = "なにがシりたい？";
-            mode = 3;
+            //mode = 3;
         } else {
         		List<Map<String, Object>> list1 = jdbc.queryForList
         				("SELECT words FROM instrument where id like " + id + ";");
